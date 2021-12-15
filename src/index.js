1096f47a7157704ec407bce522373df817a47f90
fatima-ola <fatimaolasunkanmi01@gmail.com>
2021-12-15 09:29:17 +0100

    created a tweet component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'
+
+function Tweet(){
+  return (
+    <div className="tweet">
+      <Avatar />
+      <div className="content">
+        <NameWithHandle /><Time />
+        <Message />   
+        <div className="buttons">
+          <ReplyButton />
+          <RetweetButton />
+          <LikeButton />
+          <MoreOptionsButton />
+        </div>
+      
+      </div>
+    </div>
+  );
+}
+
+function Avatar(){
+  return (
+    <img src="https://www.gravatar.com/avatar/nothing" className="avatar" alt="avatar"/>
+  )
+}
+
+function Message(){
+  return(
+    <div className="message">
+        I deployed my first React Application.
+    </div>
+  )
+}
+
+function NameWithHandle(){
+  return(
+    <span className="name-with-handle">
+      <span className="name">Your Name</span>
+      <span className="handle">@yourhandle</span>
+    </span>
+  )
+}
+
+const Time = () => <span className="time">3h ago</span>;
+
+const ReplyButton = () => <i className="fa fa-reply reply-button"/>;
+
+const RetweetButton = () => <i className="fa fa-retweet retweet-button"/>;
+
+const LikeButton = () => <i className="fa fa-heart like-button"/>;
+
+const MoreOptionsButton = () => <i className="fa fa-ellipsis-h more-options-button" />;
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Tweet />,
+  document.querySelector('#root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
